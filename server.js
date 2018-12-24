f899670b53bf53b205c7d36f46f36420a381f11c
Akanksha Kapoor <agaur05@gmail.com>
2018-12-23 18:49:11 -0600

    Adding final files

--- server.js
+++ server.js
@@ -1,57 +1,113 @@
-var express = require("express");
-var exphbs = require("express-handlebars");
-var bodyParser = require("body-parser");
-var request = require("request");
-var mongoose = require("mongoose");
-var dbModel = require("./models");
-var logger = require("morgan");
-var cheerio = require("cheerio");
-var path = require("path");
-var app = express();
-var PORT = process.env.PORT || 4000;
+const	express = require("express"),
+		bodyParser = require("body-parser"),
+		logger = require("morgan"),
+		mongoose = require("mongoose"),
+		request = require("request"),
+		cheerio = require("cheerio"),
+		PORT = process.env.PORT || 3000;
+		exphbs = require('express-handlebars'), 
+		util = require('util'),
+		jquery = require('jquery');
 
-// Parse application/x-www-form-urlencoded
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(express.static("./public"));
+var	Article = require('./models/Article.js'),
+		Note = require('./models/Note.js');
+// Mongoose mpromise deprecated - use bluebird promises
+var Promise = require("bluebird");
 
-// connect to database
 mongoose.Promise = Promise;
-var dbConnect = process.env.MONGODB_URI || "mongodb://localhost/foxsScrape";
-if(process.env.MONGODB_URI) {
-    mongoose.connect(process.env.MONGODB_URI)
-} else {
-    mongoose.connect(dbConnect);
-}
-var db = mongoose.connection;
-db.on('error',function(err){
-    console.log('Mongoose Error',err);
-});
-db.once('open', function(){
-    console.log("Mongoose connection is successful");
-});
 
-app.engine("handlebars", exphbs({
-    defaultLayout: "main"
+var app = express();
+
+app.engine('hbs', exphbs({ 
+  extname: 'hbs', 
+  defaultLayout: 'main', 
+  layoutsDir: __dirname + '/views/layouts/',
+  partialsDir: __dirname + '/views/partials/'
+}));
+app.set('view engine', 'handlebars');
+
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
 }));
 
-app.set("view engine", "handlebars");
+// Make public a static dir
+app.use(express.static("public"));
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "views/index.html"));
+// Database configuration with mongoose
+mongoose.connect(MONGODB_URI);
+var db = mongoose.connection;
+
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
 });
 
-require("./routes/scrape")(app);
-require("./routes/html.js")(app);
-
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "views/index.html"));
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
 });
 
 
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+app.use(logger('combined'));
+logger('combined', {buffer: true});
+
+request("http://www.dailykos.com", function(error, response, html) {
+
+  // Load the HTML into cheerio
+  var $ = cheerio.load(html);
+
+  // Make an empty array for saving our scraped info
+  // var result = [];
+
+  // With cheerio, look at each award-winning site, enclosed in "figure" tags with the class name "site"
+  $(".story").each(function(i, element) {
+  	var storyTitle = $(element).find(".story-title.heading").children("a").first().text();
+  	var storyDate = $(element).find(".author-date.visible-sm-block").children("span.timestamp").first().text();
+    var storyLink = $(element).find(".story-title.heading").children("a").first().attr("href");
+    var para1 = $(element).find(".story-intro").find("p").first().text();
+
+    var newArticle = new Article({
+    	title: storyTitle,
+     	date: storyDate,
+     	link: "http://www.dailykos.com" + storyLink,
+     	story: para1
+    });
+
+    newArticle.save(function(err, data) {
+    	if(err) {
+    		console.log("newarticle save error is " + err);
+    	} else {
+    		console.log(data);
+    	}
+    });
+  }); //cheerio each
+});//request
+
+app.get('/', function(req, res) {
+	var article = new Article(req.query);
+	article.retrieveAll(res);
+
+});
+
+app.get('/detail', function(req, res) {
+	var article = new Article(req.query);
+	article.retrieveOne(req, res);
+});
+
+app.get('/submit', function(req, res) {
+	var note = new Note(req.query);
+	console.log('note instance ' + note);
+	note.saveNote(req, res, Article, note);
+});
+
+app.get('/shownotes', function(req, res) {
+	var article = new Article(req.query);
+	console.log('article instance ' + article);
+	article.viewNotes(req, res, Note, article);
+});
+
+app.listen(PORT, function() {
+	console.log('app listening on port ' + PORT);
 });
